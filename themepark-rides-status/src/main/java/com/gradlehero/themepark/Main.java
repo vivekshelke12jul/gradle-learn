674868e510f3feb1fa6515a574f8f0abe59fa570
Vivek Shelke <vivekshelke12jul@gmail.com>
2026-01-17 14:35:32 +0530

    building dummy java project

---
+++ themepark-rides-status/src/main/java/com/gradlehero/themepark/Main.java
@@ -0,0 +1,46 @@
+package com.gradlehero.themepark;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("A single arg must be passed");
+            System.exit(1);
+        }
+
+        String category = args[0];
+        String example = getExample(category);
+        System.out.printf("Example of %s numbers are : %s", category, example);
+
+    }
+
+    private static String getExample(String category) {
+        List<String> numbers = readFile(String.format("%s.txt", category));
+        return String.join(", ", numbers);
+    }
+
+    private static List<String> readFile(String filename) {
+        InputStream resourceStream = Main.class.getClassLoader().getResourceAsStream(filename);
+        if (resourceStream == null) {
+            throw new IllegalArgumentException("Category not found");
+        }
+
+        List<String> result = new ArrayList<>();
+        try (BufferedReader bufferedInputStream = new BufferedReader(new InputStreamReader(resourceStream, StandardCharsets.UTF_8))) {
+            while (bufferedInputStream.ready()) {
+                result.add(bufferedInputStream.readLine());
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException("Couldn't read file", exception);
+        }
+
+        return result;
+    }
+}
